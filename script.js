fc860a1b9c4a8f21a922cf3c7393bbb5c07ac194
vidya735 <vidya.shirole@gmail.com>
2025-05-20 14:33:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function contactOnWhatsApp() {
   const message = "Hello, I'm interested in this biodata from Mahalaxmi Matrimony. Please share more details.";
-  const phoneNumber = "919812345678"; // Replace with your number
+  const phoneNumber = "8618125511"; // Replace with your number
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   window.open(url, '_blank');
 }
