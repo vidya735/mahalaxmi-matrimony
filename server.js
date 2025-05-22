b90b893cda6994a5e8f24486a6123bf16b8558be
vidya735 <vidya.shirole@gmail.com>
2025-05-22 16:42:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
